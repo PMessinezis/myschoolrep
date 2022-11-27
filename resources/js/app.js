4d24bdf2f3ea833e30c40917b7878ffc42020caf
panagiotis <panagiotis@messinezis.com>
2022-11-27 16:36:20 +0000

    vite-livewire-plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,5 @@ require('./bootstrap')
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
+livewire_hot_reload();
